56043803dfd78c1a959e772d457e443ee9a6e2c8
Can Akkaya <can@192.168.1.22>
2025-01-28 17:21:41 +0300

    login

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import Firebase // Firebase modülünü import et
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Firebase'i başlatma
+    FirebaseApp.configure()
+
+    // Flutter plugin'lerini kaydet
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
